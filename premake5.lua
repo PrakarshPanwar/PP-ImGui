6acae4a41bb61ab64a0ff2f6e516ce0dfcaa366b
PrakarshPanwar <prakarshpanwar@gmail.com>
2022-11-01 13:15:06 +0530

    Added ImGui Premake
    - This is done in order to add it as a submodule of my repository

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_impl_glfw.cpp",
+		"imgui_impl_vulkan.cpp",
+        "imgui_impl_vulkan.h",
+		"imgui_tables.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+        "ImGuizmo.cpp",
+        "ImGuizmo.h"
+	}
+
+	includedirs
+	{
+		"../glfw/include",
+        "../VulkanSDK/Include"
+    }
+
+    filter "system:windows"
+    systemversion "latest"
+	cppdialect "C++20"
+	staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
